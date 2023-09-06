97b19a625cb076aa09c260ccea461d7e7e3b524a
Bikrant Bikram Pratap Maurya <48064909+bikkimaurya@users.noreply.github.com>
2023-09-06 13:24:39 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// setting us some local verialbles
+
+const  PORT = 80 || process.env.PORT;
+
+// setting up some handels
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('*', (req, res, next) => {
+    res.render('index');
+});
+
+
+app.post('*', (req, res, next) => {
+    res.render('index');
+});
+
+
+
+app.listen(PORT, () => {
+    console.log('serever is live at  port  no: %s', PORT )
+});
